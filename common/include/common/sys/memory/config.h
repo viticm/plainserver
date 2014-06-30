6c6f008e9ba9d489bf24c6650cc43a963e108c83
viticm <viticm.ti@gmail.com>
2014-06-30 15:17:05 +0800

    share memory module add

--- common/include/common/sys/memory/config.h
+++ common/include/common/sys/memory/config.h
@@ -13,4 +13,10 @@
 
 #include "common/sys/config.h"
 
+typedef enum {
+  kCmdModelClearAll = 1,
+    kCmdModelLoadDump = 2,
+} cmd_model_t; //命令行模式
+extern int32_t g_cmd_model;
+
 #endif //PS_COMMON_SYS_MEMORY_CONFIG_H_
